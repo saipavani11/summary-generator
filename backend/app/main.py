f4759c356a0fa69d6ae4be5b0e7f022a8bd2c5bd
saipavani11 <saipavani2004@gmail.com>
2025-08-31 14:14:13 +0530

    added the route in corsmiddleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,7 +22,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://summary-generator-six.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
